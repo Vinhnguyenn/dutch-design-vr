70bf70725cd3a7d8d8fae7493857f6d8179d449d
Vinh Nguyen <thanhvinh@live.com>
2022-03-18 16:53:17 +0100

    Nieuwe versie

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ AFRAME.registerComponent("foo", {
         var model = el.object3D
 
        this.el.addEventListener("hitstart", (e)=>{
-            window.location = "index3.html"
+            window.location = "index4.html"
        })
     }
   })
@@ -19,16 +19,34 @@ AFRAME.registerComponent("button", {
     var el = this.el;
     var model = el.object3D
 
-    this.el.addEventListener("mousedown", (e)=>{
+    this.el.addEventListener("click", (e)=>{
         let obj = document.getElementsByClassName("hiddenObj")
-
         for(i=0;i<obj.length;i++){
-          obj[i].setAttribute("visible", "true")
-          let animation = "property: model-opacity; dur: 1000; to: 1" 
+          // obj[i].setAttribute("visible", "true")
+          // let animation = "property: model-opacity; dur: 1500; from: 0; to: 1; easing: easeInCubic" 
+          let animation = "property: light.intensity; dur: 1500; from: 0; to: 1.5; easing: easeInOutCubic" 
           obj[i].setAttribute("animation", animation); 
-       }
-   })
-}
+       };
+
+       let obj1 = document.getElementsByClassName("hideObj")
+        for(i=0;i<obj1.length;i++){
+          let animation = "property: position; dur: 1500; to: 0 -2 -5; delay: 500;easing: easeInOutCubic" 
+          obj1[i].setAttribute("animation", animation); 
+        };
+
+        let obj3 = document.getElementsByClassName("hideObj2")
+        for(i=0;i<obj3.length;i++){
+          let animation = "property: light.intensity; dur: 1500; from: 0; to: 0.680; easing: easeInCubic" 
+          obj3[i].setAttribute("animation", animation); 
+        };
+
+        let obj2 = document.getElementsByClassName("hideLight")
+        for(i=0;i<obj2.length;i++){
+          let animation = "property: light.intensity; delay: 500; dur: 1500; from: 1; to: 0; easing: easeInOutCubic" 
+          obj2[i].setAttribute("animation", animation); 
+        };w
+   });
+  }
 })
 
 AFRAME.registerComponent("model-opacity", {
